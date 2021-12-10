289089963d1f872809b20ffb55b8a75601ac6d73
Nicki Bo Otte <duckay@live.com>
2021-12-10 12:09:34 +0100

    Made sure that the assignments are correct in order to the specification

--- src/components/StringReverser/index.tsx
+++ src/components/StringReverser/index.tsx
@@ -1,29 +1,26 @@
 import React, { useRef } from 'react'
-import { fizzbuzzLogic } from './fizzbuzz'
 import Button from '../Button'
 import InputField from '../InputField'
 
-const Solver = () => {
+const StringReverser = () => {
 
   const inputFieldRef = useRef<HTMLInputElement>(null)
 
   const reverseString = (string:string):string => string.split('').reverse().join('')
 
-  const reverseStringDecision = (string:string):string => (Number(string))?string:reverseString(string)
-
   const handleButtonPress = () => {
     if(inputFieldRef.current !== null) {
-      inputFieldRef.current.value = reverseStringDecision(fizzbuzzLogic(+inputFieldRef.current.value))
+      inputFieldRef.current.value = reverseString(inputFieldRef.current.value)
     }
   }
 
   return (
     <div className={'space'}>
-      <h1>Fizz buzz reverse solver</h1>
+      <h1>String reverser</h1>
       <InputField innerRef={inputFieldRef} />
       <Button text={'Løs fizz buzz'} onClick={handleButtonPress} />
     </div>
   )
 }
 
-export default Solver
+export default StringReverser
